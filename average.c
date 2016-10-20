17b7a1d25d3948f94db957ae8a721dd865e0d68e
Mei Takashima <st14d23@gm.ibaraki-ct.ac.jp>
2016-10-20 15:19:44 +0900

    第１９週演習２提出

---
+++ average.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"average.h"
+
+int average(int payments[])
+{
+	int sum,ave,i;
+	sum = 0;
+	for(i=0; i<5; i++){
+		sum = sum+payments[i];
+	}
+	ave = sum/5;
+	printf("%d\n", ave);
+
+	return ave;
+}
